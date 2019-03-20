59c59c0c0edb3b6552c3797b0a575b63250d4604
= <997740098@qq.com>
2019-03-20 16:01:14 +0800

    选定了tabbar首页、薪资爆料、我的图标

---
+++ src/pages/salary/salary.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="clickHandle">
+
+  </div>
+</template>
+
+<script>
+// import card from '@/components/card'
+
+export default {
+  data () {
+    return {}
+  },
+
+  // components: {
+  //   card
+  // },
+
+  methods: {
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+  mounted () {
+    // const this_ = this
+  },
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
