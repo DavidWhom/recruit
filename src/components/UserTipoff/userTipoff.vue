1e1837ac6a1cc8c81bb546bcf14ade8f42f36562
= <997740098@qq.com>
2019-04-18 23:24:01 +0800

    修改了[用户-我的爆料]，应该显示下可信度

--- src/components/UserTipoff/userTipoff.vue
+++ src/components/UserTipoff/userTipoff.vue
@@ -6,7 +6,7 @@
           <van-col span="11">
             <div class="panel-header-number">
               <div class="before-title-blue"></div>
-              <div class="panel-title">薪资爆料 - <span class="blue-text">5 条</span></div>
+              <div class="panel-title">我的爆料 - <span class="blue-text">5 条</span></div>
             </div>
           </van-col>
         </van-row>
@@ -17,7 +17,7 @@
         <van-row>
           <div style="width: 100%;" class="van-hairline--bottom toutiao-title">
             <van-row>
-              <van-col span="8" offset="2">
+              <van-col span="6" offset="2">
                 <div class="van-ellipsis mine-title-name mine-title-list">
                   <img src="../../../static/images/mine/mine-time.png" style="height: 15px;width: 15px;"/>
                   <span>发布时间</span>
@@ -26,7 +26,13 @@
               <van-col  span="8" offset="1">
                 <div class="mine-title-list van-ellipsis">
                   <img src="../../../static/images/mine/mine-baoliao.png" style="height: 15px;width: 15px;"/>
-                  <span>薪资动态</span>
+                  <span>薪资爆料</span>
+                </div>
+              </van-col>
+              <van-col  span="5" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <img src="../../../static/images/mine/mine-believe.png" style="height: 15px;width: 15px;"/>
+                  <span>可信度</span>
                 </div>
               </van-col>
             </van-row>
@@ -36,23 +42,13 @@
               <van-col span="8" offset="1">
                 <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
               </van-col>
-              <van-col span="12">
+              <van-col span="9">
                 <div class="mine-title-list van-ellipsis"><span>拼多多 - Java后台 - 16*14</span></div>
               </van-col>
-              <van-col offset="1" span="2">
-                <div class="mine-title-content mine-title-list">
-                  <img src="../../../static/images/goal-forward.png" style="height: 20px;width: 20px;"/>
+              <van-col  span="3" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <span>5000</span>
                 </div>
-              </van-col>
-            </van-row>
-          </div>
-          <div style="width: 100%;" class="van-hairline--bottom mine-title-tr">
-            <van-row>
-              <van-col span="8" offset="1">
-                <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
-              </van-col>
-              <van-col span="12">
-                <div class="mine-title-list van-ellipsis"><span>美的电子商务 - 前端 - 10.5k*14-16</span></div>
               </van-col>
               <van-col offset="1" span="2">
                 <div class="mine-title-content mine-title-list">
@@ -61,13 +57,18 @@
               </van-col>
             </van-row>
           </div>
-          <div style="width: 100%;" class="van-hairline--bottom mine-title-tr">
+          <div style="width: 100%;" @click="showSalaryDetail()" class="van-hairline--bottom mine-title-tr">
             <van-row>
               <van-col span="8" offset="1">
                 <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
               </van-col>
-              <van-col span="12">
-                <div class="mine-title-list van-ellipsis"><span>字节跳动 - 销售管培 - 11k*15</span></div>
+              <van-col span="9">
+                <div class="mine-title-list van-ellipsis"><span>拼多多 - Java后台 - 16*14</span></div>
+              </van-col>
+              <van-col  span="3" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <span>5000</span>
+                </div>
               </van-col>
               <van-col offset="1" span="2">
                 <div class="mine-title-content mine-title-list">
@@ -76,13 +77,18 @@
               </van-col>
             </van-row>
           </div>
-          <div style="width: 100%;" class="van-hairline--bottom mine-title-tr">
+          <div style="width: 100%;" @click="showSalaryDetail()" class="van-hairline--bottom mine-title-tr">
             <van-row>
               <van-col span="8" offset="1">
                 <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
               </van-col>
-              <van-col span="12">
-                <div class="mine-title-list van-ellipsis"><span>字节跳动 - 销售管培 - 11k*15</span></div>
+              <van-col span="9">
+                <div class="mine-title-list van-ellipsis"><span>拼多多 - Java后台 - 16*14</span></div>
+              </van-col>
+              <van-col  span="3" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <span>5000</span>
+                </div>
               </van-col>
               <van-col offset="1" span="2">
                 <div class="mine-title-content mine-title-list">
@@ -91,13 +97,18 @@
               </van-col>
             </van-row>
           </div>
-          <div style="width: 100%;" class="van-hairline--bottom mine-title-tr">
+          <div style="width: 100%;" @click="showSalaryDetail()" class="van-hairline--bottom mine-title-tr">
             <van-row>
               <van-col span="8" offset="1">
                 <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
               </van-col>
-              <van-col span="12">
-                <div class="mine-title-list van-ellipsis"><span>字节跳动 - 销售管培 - 11k*15</span></div>
+              <van-col span="9">
+                <div class="mine-title-list van-ellipsis"><span>拼多多 - Java后台 - 16*14</span></div>
+              </van-col>
+              <van-col  span="3" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <span>5000</span>
+                </div>
               </van-col>
               <van-col offset="1" span="2">
                 <div class="mine-title-content mine-title-list">
@@ -106,13 +117,18 @@
               </van-col>
             </van-row>
           </div>
-          <div style="width: 100%;" class="van-hairline--bottom mine-title-tr">
+          <div style="width: 100%;" @click="showSalaryDetail()" class="van-hairline--bottom mine-title-tr">
             <van-row>
               <van-col span="8" offset="1">
                 <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
               </van-col>
-              <van-col span="12">
-                <div class="mine-title-list van-ellipsis"><span>字节跳动 - 销售管培 - 11k*15</span></div>
+              <van-col span="9">
+                <div class="mine-title-list van-ellipsis"><span>拼多多 - Java后台 - 16*14</span></div>
+              </van-col>
+              <van-col  span="3" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <span>5000</span>
+                </div>
               </van-col>
               <van-col offset="1" span="2">
                 <div class="mine-title-content mine-title-list">
@@ -121,13 +137,18 @@
               </van-col>
             </van-row>
           </div>
-          <div style="width: 100%;" class="van-hairline--bottom mine-title-tr">
+          <div style="width: 100%;" @click="showSalaryDetail()" class="van-hairline--bottom mine-title-tr">
             <van-row>
               <van-col span="8" offset="1">
                 <div class="van-ellipsis mine-title-name mine-title-list"><span>2019-4-18 18:20</span></div>
               </van-col>
-              <van-col span="12">
-                <div class="mine-title-list van-ellipsis"><span>字节跳动 - 销售管培 - 11k*15</span></div>
+              <van-col span="9">
+                <div class="mine-title-list van-ellipsis"><span>拼多多 - Java后台 - 16*14</span></div>
+              </van-col>
+              <van-col  span="3" offset="0">
+                <div class="mine-title-list van-ellipsis">
+                  <span>5000</span>
+                </div>
               </van-col>
               <van-col offset="1" span="2">
                 <div class="mine-title-content mine-title-list">
